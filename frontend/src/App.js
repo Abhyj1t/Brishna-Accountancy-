1311349050d8d90a726f461e14bf3c2aaefcff30
Abhyj1t <abhyjits@students.federation.edu.au>
2024-08-07 15:06:55 +1000

    Abhyjit has set up the frontend with a basic React project and created a Header component.

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Welcome to Brishna Accountancy</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Brishna Accountancy</h1>
+    </header>
+  );
+};
+
+export default Header;

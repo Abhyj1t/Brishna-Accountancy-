b7ac6493595ce7a3f837974b1c03834d106b0a73
Tushar98480 <30397538@students.federation.edu.au>
2024-09-17 19:48:53 +1000

    Update server.js
    some changes

--- backend/server.js
+++ backend/server.js
@@ -97,6 +97,6 @@ app.post('/api/contact', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' () => {
   console.log(`Server running on port ${PORT}`);
 });
